8e95b2780b903ce43b9d45ac67be9ffcbaa236fb
Mitch Bailey <mitchell.bailey@bmrn.com>
2019-11-11 16:54:41 -0800

    initial us infectious app

---
+++ app.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from us_infectious_toy import infections, show_year_map
+
+
+external_stylesheets = ['us_infectious.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+app.layout = html.Div(children=[
+    html.H1(children='US Infectious Disease'),
+
+    dcc.Dropdown(
+        id='year',
+        options=[{'label': x, 'value': x} for x in ['2016', '2017', '2018']],
+        value='2018'
+    ),
+    dcc.Dropdown(
+        id='infection',
+        options=[{'label': x, 'value': x} for x in infections],
+        value='Measles, Total'
+    ),
+    dcc.Dropdown(
+        id='count_type',
+        options=[{'label': x, 'value': x} for x in ['Absolute', 'Relative to State Population']],
+        value='Relative to State Population'
+    ),
+
+    dcc.Graph(
+        id='us-infectious-graph',
+    )
+])
+
+@app.callback(
+    Output('us-infectious-graph', 'figure'),
+    [Input('year', 'value'),
+     Input('infection', 'value'),
+     Input('count_type', 'value')])
+def update_figure(year, infection, count_type):
+    return show_year_map(year, infection, count_type)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ us_infectious_toy.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import json
+import urllib
+import glob
+import plotly.graph_objects as go
+
+
+infections = ['Listeriosis',
+              'Lyme disease, Total',
+              'Lyme disease, Confirmed',
+              'Lyme disease, Probable',
+              'Malaria',
+              'Measles, Total',
+              'Measles, Indigenous',
+              'Measles, Imported']
+
+font_dict = dict(
+            family="Courier New, monospace",
+            size=18,
+            color="#7f7f7f"
+            )
+
+
+def get_state_populations(simple=True):
+    def clean(x):
+        colnames = x.iloc[0, :]
+        dat = x.iloc[1:, :]
+        dat.columns = colnames
+        return dat
+    
+    census_populations = {}
+    census_population_files = glob.glob('./census_populations/*_data_*.csv')
+    for f in census_population_files:
+        year = f.split('/')[-1].split('.')[0].replace('ACSDP1Y', '')
+        temp = clean(pd.read_csv(f))
+        if simple:
+            census_populations[year] = dict(zip(temp['Geographic Area Name'],
+                                                temp['Estimate!!SEX AND AGE!!Total population'].astype(int)))
+        else:
+            census_populations[year] = temp
+    
+    return census_populations
+
+
+def clean_dat(x):
+    try:
+        x = str(x).replace(',', '')
+        num_x = float(x)
+        return num_x
+    except:
+        return None
+
+
+def get_dat(year):
+    url = f'https://wonder.cdc.gov/nndss/static/{year}/annual/{year}-table2i.txt'
+    lines = [x.decode('iso8859_15').rstrip() for x in urllib.request.urlopen(url)]
+    
+    start_line = lines.index('tab delimited data:')
+    dat = pd.DataFrame([x.rstrip().split('\t') for x in lines[start_line+1:]])
+    dat.columns = [x.rstrip() for x in lines[start_line-len(dat.columns)-1:start_line-1]]
+    dat['Reporting Area'] = dat['Reporting Area'].apply(lambda x: x if x != '' else None)
+    
+    dat['code'] = dat['Reporting Area'].apply(lambda x: codes.get(x))
+
+    for inf in infections:
+        dat[inf] = dat[inf].dropna().apply(lambda x: clean_dat(x))
+
+    return dat
+
+
+def get_state_dat(year):
+    dat = get_dat(year)
+    state_dat = dat.dropna(subset=['code'])
+    return state_dat
+
+
+def show_year_map(year='2018',
+                  infection='Measles, Total',
+                  count_type='Absolute'):
+    temp_dat = get_state_dat(year)
+    if count_type != 'Absolute':
+        temp_dat[infection] = temp_dat.apply(lambda x: x[infection]/state_populations.get(str(year)).get(x['Reporting Area'])*1e5,
+                                             axis=1)
+    fig = go.Figure(data=go.Choropleth(
+        locations=temp_dat['code'],
+        z = temp_dat[infection].astype(float),
+        locationmode = 'USA-states',
+        colorscale = 'Reds',
+        colorbar_title = f"{infection.title()} Cases ({count_type})",
+        text = temp_dat[infection].apply(lambda x: f'{x} per 100k')
+    ))
+
+    fig.update_layout(
+        title_text = f'{infection} Cases {year}',
+        geo_scope='usa',
+        font=font_dict
+    )
+
+    return fig
+
+
+state_populations = get_state_populations()
+codes = json.load(open('state_statecode.json'))
