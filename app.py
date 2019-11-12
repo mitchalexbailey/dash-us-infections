b3a6ea52f60bb7803e396915c96eb152bdceab28
Mitch Bailey <mitchell.bailey@bmrn.com>
2019-11-11 21:02:47 -0800

    minor beauty gains

--- app.py
+++ app.py
@@ -14,28 +14,42 @@ server = app.server
 # app = dash.Dash(__name__)
 
 
-app.layout = html.Div(children=[
-    html.H1(children='US Infectious Disease'),
+app.layout = html.Div(
+    className='container-fluid',
+    children=[
+    html.H1(
+        className='title',
+        children='US Infectious Disease'),
+    html.Div(
+        className='row',
+        children=[
+        html.Div(
+            className='col-xs-12',
+            children=[
+            dcc.Dropdown(
+                id='year',
+                className="dash-input",
+                options=[{'label': x, 'value': x} for x in ['2016', '2017', '2018']],
+                value='2018'
+            ),
+            dcc.Dropdown(
+                id='infection',
+                className="dash-input",
+                options=[{'label': x, 'value': x} for x in infections],
+                value='Measles, Total'
+            ),
+            dcc.Dropdown(
+                id='count_type',
+                className="dash-input",
+                options=[{'label': x, 'value': x} for x in ['Absolute', 'Relative to State Population']],
+                value='Relative to State Population'
+            ),
 
-    dcc.Dropdown(
-        id='year',
-        options=[{'label': x, 'value': x} for x in ['2016', '2017', '2018']],
-        value='2018'
-    ),
-    dcc.Dropdown(
-        id='infection',
-        options=[{'label': x, 'value': x} for x in infections],
-        value='Measles, Total'
-    ),
-    dcc.Dropdown(
-        id='count_type',
-        options=[{'label': x, 'value': x} for x in ['Absolute', 'Relative to State Population']],
-        value='Relative to State Population'
-    ),
+            dcc.Graph(
+                id='us-infectious-graph',
+            )])
 
-    dcc.Graph(
-        id='us-infectious-graph',
-    )
+    ])
 ])
 
 @app.callback(
